d97ed62d0db0bb7418e73b2d0d7458870278f6bd
VanyaMate <buhu653@mail.ru>
2023-12-18 23:04:29 +0300

    pre

--- src/api/v1/api-v1.router.ts
+++ src/api/v1/api-v1.router.ts
@@ -1,9 +1,16 @@
 import * as express from 'express';
 import { Router } from 'express';
+import { AuthService } from '../../services/auth/auth.service';
+import { UserPrismaRepository } from '../../repository/user/user-prisma.repository';
+import { PrismaClient } from '@prisma/client';
 
 
 export const apiV1Router: Router = express.Router();
+const prisma = new PrismaClient()();
 
 
-apiV1Router.use('auth', () => {
+apiV1Router.use('auth', (req, res) => {
+    new AuthService(
+        new UserPrismaRepository(prisma)
+    )
 });
